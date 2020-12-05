b614bda4386c19c06e00c650618c65351ebbf91a
Mokshith-dev <mokshith.ajalade@gmail.com>
2020-12-05 18:31:30 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const express = require('express')
+const express = require('express');
+const { connection } = require('mongoose');
 const app = express()
 const port = 8080
-const data = require('./data');
-const onePageArticleCount = 10
 
+const onePageArticleCount = 10
+const {newsArticleModel} = require('./connector');
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/newFeeds',(req,res) => {
+app.get('/newFeeds', async (req,res) => {
     let limit = req.body.limit;
     let offset = req.body.offset;
     if(!limit) {
@@ -18,7 +19,9 @@ app.get('/newFeeds',(req,res) => {
     if(!offset) {
         offset=0;
     }
+
     let resArr = [];
+    const data = await newsArticleModel.find();
     resArr = data.slice(offset,offset+limit);
     res.send(resArr);
 
